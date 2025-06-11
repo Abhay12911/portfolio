934c8bdb1a8442ac715845e1312a543e373056ea
Abhay12911 <abhaystp30@gmail.com>
2025-06-12 00:09:32 +0530

    nav

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navbar from './components/common/Navbar';
 import HomePage from './components/pages/HomePage';
+import Navbar from './components/common/Navbar'
 // import AboutPage from './components/pages/AboutPage';
 import ContactPage from './components/pages/Contactpage';
 import ProjectsPage from './components/pages/ProjectsPage';
